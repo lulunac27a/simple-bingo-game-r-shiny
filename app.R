1f4c5c979144c4899568f0a0cca0597ac38e220d
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-04-15 21:24:10 -0500

    Add comments

--- app.R
+++ app.R
@@ -1,9 +1,14 @@
+# import Shiny library
 library(shiny)
-
+# define UI component
 ui <- fluidPage(titlePanel("Simple Bingo Game in R Shiny"), sidebarLayout(sidebarPanel(actionButton("start",
-  "Start Game")), mainPanel(tableOutput("bingoGrid"))))
+  "Start Game")  #button to start game
+), mainPanel(tableOutput("bingoGrid")  #bingo grid table output
+)))
 
 server <- function(input, output) {
+  # define server component when start button is pressed
+  # generate random numbers
   observeEvent(input$start, {
     b <- sample(1:15, 5)
     i <- sample(16:30, 5)
@@ -11,6 +16,7 @@ server <- function(input, output) {
     g <- sample(46:60, 5)
     o <- sample(61:75, 5)
     output$bingoGrid <- renderTable({
+      # render bingo grid table
       bingo_table <- matrix(c(b, i, n, g, o), nrow = 5,
         ncol = 5, byrow = TRUE)
       colnames(bingo_table) <- c("B", "I", "N", "G", "O")
@@ -19,4 +25,4 @@ server <- function(input, output) {
   })
 }
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)  #run the Shiny web app server
