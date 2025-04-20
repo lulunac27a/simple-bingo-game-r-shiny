f3a8b3d4ddf084a541c6f7b4ed483d3240ea4b28
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-04-19 20:05:20 -0500

    Fix and improve code

--- app.R
+++ app.R
@@ -23,49 +23,28 @@ server <- function(input, output, session) {
     called_number <- reactiveValues(value = 0)
     number_list <- sample(1:75, 75, replace = FALSE)
     bingo_filled <- matrix(FALSE, nrow = 5, ncol = 5) # fill bingo grid with false values (not filled)
-    b <- sample(1:15, 5, replace = FALSE)
-    i <- sample(16:30, 5, replace = FALSE)
-    n <- sample(31:45, 5, replace = FALSE)
-    g <- sample(46:60, 5, replace = FALSE)
-    o <- sample(61:75, 5, replace = FALSE)
-
-    observeEvent(input$start, {
-        called_numbers <- 0
-        called_number$value <- 0
-        invalidateLater(1000, session)
-        # when start button is pressed generate random numbers
+    generate_bingo_table <- function() {
+        # function to generate bingo table with random numbers
         b <- sample(1:15, 5, replace = FALSE)
         i <- sample(16:30, 5, replace = FALSE)
         n <- sample(31:45, 5, replace = FALSE)
         g <- sample(46:60, 5, replace = FALSE)
         o <- sample(61:75, 5, replace = FALSE)
-        output$bingoGrid <- renderTable({
-            # render bingo grid table
-            bingo_table <- matrix(
-                c(b, i, n, g, o),
-                nrow = 5,
-                ncol = 5,
-                byrow = FALSE
-            ) # bingo table
-            colnames(bingo_table) <- c("B", "I", "N", "G", "O") # column names
-            bingo_table
-        })
-        output$bingoGridTable <- renderDT({
-            # render bingo grid data table
-            bingo_table <- matrix(
-                c(b, i, n, g, o),
-                nrow = 5,
-                ncol = 5,
-                byrow = FALSE
-            ) # bingo table
-            colnames(bingo_table) <- c("B", "I", "N", "G", "O") # column names
-            datatable(bingo_table)
-        })
+        matrix(c(b, i, n, g, o), nrow = 5, ncol = 5, byrow = FALSE)
+    }
+    bingo_table <- reactiveVal(generate_bingo_table())
+
+    observeEvent(input$start, {
+        bingo_filled[, ] <- FALSE
+        bingo_table(generate_bingo_table()) # when start button is pressed generate random numbers
+        called_numbers <- 0
+        called_number$value <- 0
+        invalidateLater(1000, session)
     })
 
     observeEvent(input$bingoGrid_cell_clicked, {
-        row <- input$bingoGrid_cell_clicked$row
-        col <- input$bingoGrid_cell_clicked$col
+        row <- input$bingoGrid_cell_clicked[1]
+        col <- input$bingoGrid_cell_clicked[2]
         if (called_number$value == bingo_table[row, col]) {
             bingo_filled[row, col] <<- TRUE
             output$bingoGrid <- renderTable({
@@ -101,18 +80,12 @@ server <- function(input, output, session) {
     })
 
     output$bingoGrid <- renderTable({
-        bingo_table <- matrix(
-            c(b, i, n, g, o),
-            nrow = 5,
-            ncol = 5,
-            byrow = FALSE
-        ) # bingo table
-        colnames(bingo_table) <- c("B", "I", "N", "G", "O") # column names
-        for (i in seq_len(nrow(bingo_table))) {
+        table <- bingo_table() # bingo table
+        for (i in seq_len(nrow(table))) {
             # repeat for each row in bingo table grid repeat for
             # each column in bingo table grid if bingo grid cell
             # is filled
-            for (j in seq_len(ncol(bingo_table))) {
+            for (j in seq_len(ncol(table))) {
                 if (bingo_filled[i, j]) {
                     bingo_table[i, j] <- sprintf(
                         "<span style='background-color: $0080ff'>(%s) X</span>",
@@ -121,22 +94,17 @@ server <- function(input, output, session) {
                 }
             }
         }
-        bingo_table
+        colnames(table) <- c("B", "I", "N", "G", "O") # column names
+        table
     })
     output$bingoGridTable <- renderDT({
         # render bingo grid data table
-        bingo_table <- matrix(
-            c(b, i, n, g, o),
-            nrow = 5,
-            ncol = 5,
-            byrow = FALSE
-        ) # bingo table
-        colnames(bingo_table) <- c("B", "I", "N", "G", "O") # column names
-        for (i in seq_len(nrow(bingo_table))) {
+        table <- bingo_table() # bingo table
+        for (i in seq_len(nrow(table))) {
             # repeat for each row in bingo table grid repeat for
             # each column in bingo table grid if bingo grid cell
             # is filled
-            for (j in seq_len(ncol(bingo_table))) {
+            for (j in seq_len(ncol(table))) {
                 if (bingo_filled[i, j]) {
                     bingo_table[i, j] <- sprintf(
                         "<span style='background-color: $0080ff'>(%s) X</span>",
@@ -145,7 +113,8 @@ server <- function(input, output, session) {
                 }
             }
         }
-        datatable(bingo_table)
+        colnames(table) <- c("B", "I", "N", "G", "O") # column names
+        datatable(table)
     })
 
     output$calledNumber <- renderText({
